fa178d754f5833c261abd0f5e895cfe25ec0783c
devipebiyanti13 <devifebiyanti75@gmail.com>
2026-05-11 13:05:31 +0700

    change databaseurl

--- koneksi.py
+++ koneksi.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 def get_connection():
     conn = psycopg2.connect(
-        st.secrets["postgresql://neondb_owner:npg_Rp8IWxtwn7gU@ep-twilight-base-ao1uwnfy-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"]
+        st.secrets["DATABASE_URL"]
     )
     return conn
 
