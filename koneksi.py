6bb77c095e55d296c9a2d2880bfe97d61680af37
devipebiyanti13 <devifebiyanti75@gmail.com>
2026-05-11 13:01:21 +0700

    connect app to neon database

--- koneksi.py
+++ koneksi.py
@@ -1,11 +1,9 @@
 import psycopg2
+import streamlit as st
 
 def get_connection():
     conn = psycopg2.connect(
-        host="localhost",
-        database="db_rekomendasi_dosen",
-        user="postgres",
-        password="12345",
-        port="5432"
+        st.secrets["postgresql://neondb_owner:npg_Rp8IWxtwn7gU@ep-twilight-base-ao1uwnfy-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"]
     )
     return conn
+
